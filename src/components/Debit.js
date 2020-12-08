5c5ae98551c00d54508b22d7c039d34342b4a813
phong.do <dovanphong96@gmail.com>
2020-12-08 22:06:46 +0700

    tab

--- src/components/Debit.js
+++ src/components/Debit.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import mcontext from '../Context'
 import MenuBar from './MenuBar'
 import BenPhai from './BenPhai'
@@ -6,37 +6,32 @@ import BenTren from './BenTren'
 import BenDuoi from './BenDuoi'
 
 export default function (props) {
-    let context = useContext(mcontext)
-    return /*user?*/(
-        <div className='container col-md-12'>
-            <MenuBar user={context.user} />
-            <div className='row  mt-2' style={{height: '500px'}}>
-                <div className='col-md-4' id='bentrai'>
-                    <BenTren />
-                    <BenDuoi />
-                </div>
-                <div className='col-md-8' id='benphai'>
-                    <BenPhai />
-                </div>
-            </div>
-            <ul class="nav nav-tabs" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" href="#profile" role="tab" data-toggle="tab">profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#buzz" role="tab" data-toggle="tab">buzz</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#references" role="tab" data-toggle="tab">references</a>
-  </li>
-</ul>
-
-<div class="tab-content">
-  <div role="tabpanel" class="tab-pane fade in active" id="profile">...</div>
-  <div role="tabpanel" class="tab-pane fade" id="buzz">bbb</div>
-  <div role="tabpanel" class="tab-pane fade" id="references">ccc</div>
-</div>
+  let context = useContext(mcontext)
+  return /*user?*/(
+    <div className='container col-md-12'>
+      <MenuBar user={context.user} />
+      <div className='row  mt-2' style={{ height: '500px' }}>
+        <div className='col-md-4' id='bentrai'>
+          <BenTren />
+          <BenDuoi />
         </div>
-    )/*:<Redirect to='/login'/>*/
+        {/* <div className='col-md-8' id='benphai'>
+          <BenPhai />
+        </div> */}
+        <div className="tab-content col-md-8" id='benphai'>
+          <div role="tabpanel" className="tab-pane fade" id="profile"> <BenPhai /> </div>
+          <div role="tabpanel" className="tab-pane fade in active" id="buzz">Gioi thieu</div>
+          <div role="tabpanel" className="tab-pane fade" id="references">Danh sach co</div>
+        </div>
+      </div>
+      
+
+      {/* <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade in active" id="profile">...</div>
+        <div role="tabpanel" class="tab-pane fade" id="buzz">bbb</div>
+        <div role="tabpanel" class="tab-pane fade" id="references">ccc</div>
+      </div> */}
+    </div>
+  )/*:<Redirect to='/login'/>*/
 }
 
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // import Context from '../Context';
 export default function menuBar({ user }) {
-    const logOut = function() {
+    const logOut = function () {
         localStorage.clear();
         window.location.href = '/';
     }
-    const dsCo = function(e) {
+    const dsCo = function (e) {
         e.preventDefault()
         let element = document.getElementById('id-menu')
         let liElement = element.getElementsByTagName('li')
         const leng = liElement.length
-        for (let i=0; i<leng; i++) {
+        for (let i = 0; i < leng; i++) {
             liElement[i].classList.remove('active')
         }
         // element.classList.remove('active')
@@ -26,14 +26,15 @@ export default function menuBar({ user }) {
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="nav nav-tabs mr-auto" id='id-menu'>
                     <li className="nav-item">
-                        <a className="nav-link active" href="#profile" role="tab" data-toggle="tab" id="tab1">Vay/nợ <span className="sr-only">(current)</span></a>
+                        <a className="nav-link active" role="tab" data-toggle="tab" id="tab3" href="#references">Giới thiệu</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#profile" role="tab" data-toggle="tab" id="tab1">Vay/nợ <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" role="tab" data-toggle="tab" href="#buzz" id="tab2">Danh sách cỗ</a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" role="tab" data-toggle="tab" id="tab3" href="#references">Giới thiệu</a>
-                    </li>
+
                 </ul>
                 <span className="navbar-text">
 
@@ -41,7 +42,7 @@ export default function menuBar({ user }) {
                 <ul className="navbar-nav">
                     <li className="dropdown">
                         <a className="dropdown-toggle dropdown-menu-left" data-toggle="dropdown" href="#">Xin chào {user.first_name}
-                        <span className="caret"></span>
+                            <span className="caret"></span>
                         </a>
                         <ul className="dropdown-menu">
                             <li><a href="#" onClick={logOut}>Đăng xuất</a></li>
